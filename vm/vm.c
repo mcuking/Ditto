6a7f3ff2cfed444174b80b99096b1346d01dd304
mcuking <mcuking.tang@gmail.com>
2020-08-17 14:16:53 +0800

    feat: 实现对象头

--- vm/vm.c
+++ vm/vm.c
@@ -4,8 +4,12 @@
 // 初始化一个虚拟机
 void initVM(VM *vm)
 {
-    vm->allocatedBytes = 0; // 记录已经分配的内存总和
-    vm->curLexer = NULL;    // 当前词法分析器初始化为 NULL
+    // 记录已经分配的内存总和
+    vm->allocatedBytes = 0;
+    // 当前词法分析器初始化为 NULL
+    vm->curLexer = NULL;
+    // 指向所有已分配对象链表的首节点，用于垃圾回收
+    vm->allObjects = NULL;
 };
 
 // 创建一个新的虚拟机
--- vm/vm.h
+++ vm/vm.h
@@ -1,13 +1,21 @@
 #ifndef _VM_VM_H
 #define _VM_VM_H
 #include "common.h"
+#include "header_obj.h"
 
 struct vm
 {
-    uint32_t allocatedBytes; // 记录已经分配的内存总和
-    Lexer *curLexer;         // 当前词法分析器
+    Class *stringClass;
+
+    // 记录已经分配的内存总和
+    uint32_t allocatedBytes;
+    // 当前词法分析器
+    Lexer *curLexer;
+    // 指向所有已分配对象链表的首节点，用于垃圾回收
+    ObjHeader *allObjects;
 };
 
 void initVM(VM *vm);
 VM *newVM(void);
+
 #endif
