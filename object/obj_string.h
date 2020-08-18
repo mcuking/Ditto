edb61bddd1472f71afa22e4f22e885dc178792e8
mcuking <tangjianghong@163.com>
2020-08-18 09:59:50 +0800

    feat: 定义字符串对象结构，并实现新建字符串对象方法

---
+++ object/obj_string.h
@@ -0,0 +1,22 @@
+#ifndef _OBJECT_OBJ_STRING
+#define _OBJECT_OBJ_STRING
+#include "header_obj.h"
+
+// 定义字符串对象结构
+typedef struct
+{
+    ObjHeader objHeader; // 对象头
+    CharValue value;     // 字符串值
+    uint32_t hashCode;   // 由字符串值计算的哈希值
+} ObjString;
+
+// 将字符串值根据 fnv-1a 算法转成对应哈希值
+uint32_t hashString(char *str, uint32_t length);
+
+// 根据字符串对象中的值设置对应的哈希值
+void hashObjString(ObjString *objString);
+
+// 新建字符串对象
+ObjString *newObjString(VM *vm, const char *str, uint32_t length);
+
+#endif
