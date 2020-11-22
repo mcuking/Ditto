4719676b91b67e509d4ecd38d3ea3f97559fa6a9
mcuking <tangjianghong@163.com>
2020-11-22 11:39:00 +0800

    feat: 梳理解释器流程，添加了编译核心模块、执行模块等环节

--- vm/vm.h
+++ vm/vm.h
@@ -3,6 +3,14 @@
 #include "common.h"
 #include "header_obj.h"
 #include "obj_thread.h"
+#include "obj_map.h"
+
+// 虚拟机执行结果
+typedef enum vmResult
+{
+    VM_RESULT_SUCESS,
+    VM_RESULT_ERROR
+} VMResult;
 
 struct vm
 {
@@ -18,10 +26,12 @@ struct vm
     Class *numClass;
     Class *threadClass;
 
-    uint32_t allocatedBytes; // 累计已分配的内存总和
-    ObjHeader *allObjects;   // 累计已分配的所有对象的链表（用于垃圾回收）
-    Lexer *curLexer;         // 当前词法分析器
-    ObjThread *curThread;    // 当前正在执行的线程
+    uint32_t allocatedBytes;    // 累计已分配的内存总和
+    ObjHeader *allObjects;      // 累计已分配的所有对象的链表（用于垃圾回收）
+    Lexer *curLexer;            // 当前词法分析器
+    ObjThread *curThread;       // 当前正在执行的线程
+    ObjMap *allModules;         // 所有模块
+    SymbolTable allMethodNames; // 所有类的方法
 };
 
 // 初始化虚拟机
