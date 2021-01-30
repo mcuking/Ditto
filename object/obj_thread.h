2efc69bbff643529380c64d80f76be9a6a3ef68c
mcuking <tangjianghong@163.com>
2021-01-30 13:35:29 +0800

    feat: 添加 为线程 objThread 中运行的闭包函数 objClosure 准备帧栈 Frame 的方法

--- object/obj_thread.h
+++ object/obj_thread.h
@@ -16,10 +16,9 @@ typedef struct {
 
     ObjUpvalue *openUpvalues; // TODO: 暂时未搞懂，后面填坑
 
-    struct objThread *caller; // 当前线程（thread）对象的调用者，若当前线程推出，则将控制权交回调用者（调用者本身也是一个线程对象）
+    struct objThread *caller; // 当前线程（thread）对象的调用者，若当前线程退出，则将控制权交回调用者（调用者本身也是一个线程对象）
 
     Value errorObj; // 导致运行时错误的对象会放在这里，否则为空
-
 } ObjThread;
 
 // 为线程 objThread 中运行的闭包函数 objClosure 准备运行时栈
